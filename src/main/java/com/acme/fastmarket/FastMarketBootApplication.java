208eb672183d97a0ef9f6a34324258a4389d222d
Richard <u20181c448@upc.edu.pe>
2020-11-06 01:24:53 -0500

    1st commit
    ahora ps

---
+++ src/main/java/com/acme/fastmarket/FastMarketBootApplication.java
@@ -0,0 +1,13 @@
+package com.acme.fastmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastMarketBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastMarketBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acme/fastmarket/FastMarketBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.fastmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastMarketBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
